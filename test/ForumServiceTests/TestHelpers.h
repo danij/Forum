58027c5ae072db8a6680a85e9b93ceb0ef4561dc
Dani J <dani.user@gmail.com>
2016-09-23 20:18:50 +0300

    Simplified status checks

--- test/ForumServiceTests/TestHelpers.h
+++ test/ForumServiceTests/TestHelpers.h
@@ -1,9 +1,22 @@
 #pragma once
 
+#include <boost/test/unit_test.hpp>
+
+#include "Repository.h"
+
 namespace Forum
 {
     namespace Helpers
     {
+        inline void assertStatusCodeEqual(Forum::Repository::StatusCode expected, Forum::Repository::StatusCode actual)
+        {
+            BOOST_REQUIRE_EQUAL(expected, actual);
+        }
 
+        template <typename TStatusObj>
+        inline void assertStatusCodeEqual(Forum::Repository::StatusCode expected, const TStatusObj& obj)
+        {
+            assertStatusCodeEqual(expected, Forum::Repository::StatusCode(obj.template get<uint32_t>("status")));
+        }
     }
 }
--- test/ForumServiceTests/UserTests.cpp
+++ test/ForumServiceTests/UserTests.cpp
@@ -19,96 +19,96 @@ BOOST_AUTO_TEST_CASE( Creating_a_user_with_no_parameters_fails )
 {
     auto handler = createCommandHandler();
     auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER);
-    BOOST_REQUIRE_EQUAL(int(StatusCode::INVALID_PARAMETERS), versionObj.get<int>("status"));
+    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, versionObj);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_empty_name_fails )
 {
     auto handler = createCommandHandler();
     auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "" });
-    BOOST_REQUIRE_EQUAL(int(StatusCode::INVALID_PARAMETERS), versionObj.get<int>("status"));
+    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, versionObj);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_only_whitespace_in_the_name_fails )
 {
     auto handler = createCommandHandler();
     auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { " \t\r\n" });
-    BOOST_REQUIRE_EQUAL(int(StatusCode::INVALID_PARAMETERS), versionObj.get<int>("status"));
+    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, versionObj);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_leading_whitespace_in_the_name_fails )
 {
     auto handler = createCommandHandler();
     auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { " Foo" });
-    BOOST_REQUIRE_EQUAL(int(StatusCode::INVALID_PARAMETERS), versionObj.get<int>("status"));
+    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, versionObj);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_trailing_whitespace_in_the_name_fails )
 {
     auto handler = createCommandHandler();
     auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "Foo\t" });
-    BOOST_REQUIRE_EQUAL(int(StatusCode::INVALID_PARAMETERS), versionObj.get<int>("status"));
+    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, versionObj);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_leading_nonletter_nonnumber_in_the_name_fails )
 {
     auto handler = createCommandHandler();
     auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { ":Foo" });
-    BOOST_REQUIRE_EQUAL(int(StatusCode::INVALID_PARAMETERS), versionObj.get<int>("status"));
+    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, versionObj);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_trailing_nonletter_nonnumber_in_the_name_fails )
 {
     auto handler = createCommandHandler();
     auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "Foo?" });
-    BOOST_REQUIRE_EQUAL(int(StatusCode::INVALID_PARAMETERS), versionObj.get<int>("status"));
+    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, versionObj);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_whitespace_in_the_middle_of_the_name_succeeds )
 {
     auto handler = createCommandHandler();
     auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "Foo Bar" });
-    BOOST_REQUIRE_EQUAL(int(StatusCode::OK), versionObj.get<int>("status"));
+    assertStatusCodeEqual(StatusCode::OK, versionObj);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_dash_in_the_middle_of_the_name_succeeds )
 {
     auto handler = createCommandHandler();
     auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "Foo-Bar" });
-    BOOST_REQUIRE_EQUAL(int(StatusCode::OK), versionObj.get<int>("status"));
+    assertStatusCodeEqual(StatusCode::OK, versionObj);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_underscore_in_the_middle_of_the_name_succeeds )
 {
     auto handler = createCommandHandler();
     auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "Foo_Bar" });
-    BOOST_REQUIRE_EQUAL(int(StatusCode::OK), versionObj.get<int>("status"));
+    assertStatusCodeEqual(StatusCode::OK, versionObj);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_newline_in_the_middle_of_the_name_fails )
 {
     auto handler = createCommandHandler();
     auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "Foo\nBar" });
-    BOOST_REQUIRE_EQUAL(int(StatusCode::INVALID_PARAMETERS), versionObj.get<int>("status"));
+    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, versionObj);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_strange_character_in_the_middle_of_the_name_fails )
 {
     auto handler = createCommandHandler();
     auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "Foo☂Bar" });
-    BOOST_REQUIRE_EQUAL(int(StatusCode::INVALID_PARAMETERS), versionObj.get<int>("status"));
+    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, versionObj);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_only_numbers_in_the_name_succeeds )
 {
     auto handler = createCommandHandler();
     auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "0123456789" });
-    BOOST_REQUIRE_EQUAL(int(StatusCode::OK), versionObj.get<int>("status"));
+    assertStatusCodeEqual(StatusCode::OK, versionObj);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_accented_letters_in_the_name_succeeds )
 {
     auto handler = createCommandHandler();
     auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "FȭǬ" });
-    BOOST_REQUIRE_EQUAL(int(StatusCode::OK), versionObj.get<int>("status"));
+    assertStatusCodeEqual(StatusCode::OK, versionObj);
 }
