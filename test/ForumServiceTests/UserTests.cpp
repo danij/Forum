959a0d00c95c11df72c717f76c409d768cb25f23
Dani J <dani.user@gmail.com>
2016-09-23 20:21:09 +0300

    Cleaned up user tests

--- test/ForumServiceTests/UserTests.cpp
+++ test/ForumServiceTests/UserTests.cpp
@@ -10,105 +10,90 @@ using Forum::Repository::StatusCode;
 
 BOOST_AUTO_TEST_CASE( User_count_is_initially_zero )
 {
-    auto handler = createCommandHandler();
-    auto versionObj = handlerToObj(handler, Forum::Commands::COUNT_USERS);
-    BOOST_REQUIRE_EQUAL(0, versionObj.get<int>("count"));
+    auto returnObject = handlerToObj(createCommandHandler(), Forum::Commands::COUNT_USERS);
+    BOOST_REQUIRE_EQUAL(0, returnObject.get<int>("count"));
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_no_parameters_fails )
 {
-    auto handler = createCommandHandler();
-    auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER);
-    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, versionObj);
+    auto returnObject = handlerToObj(createCommandHandler(), Forum::Commands::ADD_USER);
+    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, returnObject);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_empty_name_fails )
 {
-    auto handler = createCommandHandler();
-    auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "" });
-    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, versionObj);
+    auto returnObject = handlerToObj(createCommandHandler(), Forum::Commands::ADD_USER, { "" });
+    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, returnObject);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_only_whitespace_in_the_name_fails )
 {
-    auto handler = createCommandHandler();
-    auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { " \t\r\n" });
-    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, versionObj);
+    auto returnObject = handlerToObj(createCommandHandler(), Forum::Commands::ADD_USER, { " \t\r\n" });
+    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, returnObject);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_leading_whitespace_in_the_name_fails )
 {
-    auto handler = createCommandHandler();
-    auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { " Foo" });
-    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, versionObj);
+    auto returnObject = handlerToObj(createCommandHandler(), Forum::Commands::ADD_USER, { " Foo" });
+    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, returnObject);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_trailing_whitespace_in_the_name_fails )
 {
-    auto handler = createCommandHandler();
-    auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "Foo\t" });
-    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, versionObj);
+    auto returnObject = handlerToObj(createCommandHandler(), Forum::Commands::ADD_USER, { "Foo\t" });
+    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, returnObject);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_leading_nonletter_nonnumber_in_the_name_fails )
 {
-    auto handler = createCommandHandler();
-    auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { ":Foo" });
-    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, versionObj);
+    auto returnObject = handlerToObj(createCommandHandler(), Forum::Commands::ADD_USER, { ":Foo" });
+    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, returnObject);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_trailing_nonletter_nonnumber_in_the_name_fails )
 {
-    auto handler = createCommandHandler();
-    auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "Foo?" });
-    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, versionObj);
+    auto returnObject = handlerToObj(createCommandHandler(), Forum::Commands::ADD_USER, { "Foo?" });
+    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, returnObject);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_whitespace_in_the_middle_of_the_name_succeeds )
 {
-    auto handler = createCommandHandler();
-    auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "Foo Bar" });
-    assertStatusCodeEqual(StatusCode::OK, versionObj);
+    auto returnObject = handlerToObj(createCommandHandler(), Forum::Commands::ADD_USER, { "Foo Bar" });
+    assertStatusCodeEqual(StatusCode::OK, returnObject);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_dash_in_the_middle_of_the_name_succeeds )
 {
-    auto handler = createCommandHandler();
-    auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "Foo-Bar" });
-    assertStatusCodeEqual(StatusCode::OK, versionObj);
+    auto returnObject = handlerToObj(createCommandHandler(), Forum::Commands::ADD_USER, { "Foo-Bar" });
+    assertStatusCodeEqual(StatusCode::OK, returnObject);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_underscore_in_the_middle_of_the_name_succeeds )
 {
-    auto handler = createCommandHandler();
-    auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "Foo_Bar" });
-    assertStatusCodeEqual(StatusCode::OK, versionObj);
+    auto returnObject = handlerToObj(createCommandHandler(), Forum::Commands::ADD_USER, { "Foo_Bar" });
+    assertStatusCodeEqual(StatusCode::OK, returnObject);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_newline_in_the_middle_of_the_name_fails )
 {
-    auto handler = createCommandHandler();
-    auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "Foo\nBar" });
-    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, versionObj);
+    auto returnObject = handlerToObj(createCommandHandler(), Forum::Commands::ADD_USER, { "Foo\nBar" });
+    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, returnObject);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_strange_character_in_the_middle_of_the_name_fails )
 {
-    auto handler = createCommandHandler();
-    auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "Foo☂Bar" });
-    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, versionObj);
+    auto returnObject = handlerToObj(createCommandHandler(), Forum::Commands::ADD_USER, { "Foo☂Bar" });
+    assertStatusCodeEqual(StatusCode::INVALID_PARAMETERS, returnObject);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_only_numbers_in_the_name_succeeds )
 {
-    auto handler = createCommandHandler();
-    auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "0123456789" });
-    assertStatusCodeEqual(StatusCode::OK, versionObj);
+    auto returnObject = handlerToObj(createCommandHandler(), Forum::Commands::ADD_USER, { "0123456789" });
+    assertStatusCodeEqual(StatusCode::OK, returnObject);
 }
 
 BOOST_AUTO_TEST_CASE( Creating_a_user_with_accented_letters_in_the_name_succeeds )
 {
-    auto handler = createCommandHandler();
-    auto versionObj = handlerToObj(handler, Forum::Commands::ADD_USER, { "FȭǬ" });
-    assertStatusCodeEqual(StatusCode::OK, versionObj);
+    auto returnObject = handlerToObj(createCommandHandler(), Forum::Commands::ADD_USER, { "FȭǬ" });
+    assertStatusCodeEqual(StatusCode::OK, returnObject);
 }
