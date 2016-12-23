2755080673d83e6a73f303fdafb566648cd0b9b7
Dani J <dani.user@gmail.com>
2016-12-23 16:23:04 +0200

    Added full name for test helper constant

--- test/ForumServiceTests/TestHelpers.cpp
+++ test/ForumServiceTests/TestHelpers.cpp
@@ -16,6 +16,6 @@ bool Forum::Helpers::treeContains(const boost::property_tree::ptree& tree, const
 }
 
 const std::string Forum::Helpers::sampleValidIdString = "00000000-0000-0000-0000-000000000001";
-const Forum::Entities::IdType Forum::Helpers::sampleValidId = sampleValidIdString;
+const Forum::Entities::IdType Forum::Helpers::sampleValidId = Forum::Helpers::sampleValidIdString;
 
 const std::string Forum::Helpers::sampleMessageContent = "abcdefghijklmnopqrstuvwxyz";
