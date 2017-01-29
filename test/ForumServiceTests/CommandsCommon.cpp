e72cdc5175b1f1ff657a3d627833a2ea49062c6d
Dani J <dani.user@gmail.com>
2017-01-29 13:40:15 +0200

    Pass command handler by reference in tests to avoid useless reference count increase/decrease on shared pointer

--- test/ForumServiceTests/CommandsCommon.cpp
+++ test/ForumServiceTests/CommandsCommon.cpp
@@ -19,7 +19,7 @@ std::shared_ptr<CommandHandler> Forum::Helpers::createCommandHandler()
     return std::make_shared<CommandHandler>(memoryRepository, memoryRepository, metricsRepository);
 }
 
-std::string Forum::Helpers::handlerToString(CommandHandlerRef handler, Command command,
+std::string Forum::Helpers::handlerToString(CommandHandlerRef& handler, Command command,
                                             const std::vector<std::string>& parameters)
 {
     std::stringstream stream;
@@ -27,12 +27,12 @@ std::string Forum::Helpers::handlerToString(CommandHandlerRef handler, Command c
     return stream.str();
 }
 
-std::string Forum::Helpers::handlerToString(CommandHandlerRef handler, Command command)
+std::string Forum::Helpers::handlerToString(CommandHandlerRef& handler, Command command)
 {
     return handlerToString(handler, command, {});
 }
 
-boost::property_tree::ptree Forum::Helpers::handlerToObj(CommandHandlerRef handler, Command command,
+boost::property_tree::ptree Forum::Helpers::handlerToObj(CommandHandlerRef& handler, Command command,
                                                          const std::vector<std::string>& parameters)
 {
     std::stringstream stream;
@@ -42,7 +42,7 @@ boost::property_tree::ptree Forum::Helpers::handlerToObj(CommandHandlerRef handl
     return result;
 }
 
-boost::property_tree::ptree Forum::Helpers::handlerToObj(CommandHandlerRef handler, Command command,
+boost::property_tree::ptree Forum::Helpers::handlerToObj(CommandHandlerRef& handler, Command command,
                                                          DisplaySettings displaySettings,
                                                          const std::vector<std::string>& parameters)
 {
@@ -61,60 +61,60 @@ boost::property_tree::ptree Forum::Helpers::handlerToObj(CommandHandlerRef handl
     return handlerToObj(handler, command, parameters);
 }
 
-boost::property_tree::ptree Forum::Helpers::handlerToObj(CommandHandlerRef handler, Command command)
+boost::property_tree::ptree Forum::Helpers::handlerToObj(CommandHandlerRef& handler, Command command)
 {
     return handlerToObj(handler, command, DisplaySettings{});
 }
 
-boost::property_tree::ptree Forum::Helpers::handlerToObj(CommandHandlerRef handler, Command command, 
+boost::property_tree::ptree Forum::Helpers::handlerToObj(CommandHandlerRef& handler, Command command, 
                                                          DisplaySettings displaySettings)
 {
     return handlerToObj(handler, command, displaySettings, {});
 }
 
-std::string Forum::Helpers::createUserAndGetId(CommandHandlerRef handler, const std::string& name)
+std::string Forum::Helpers::createUserAndGetId(CommandHandlerRef& handler, const std::string& name)
 {
     auto result = handlerToObj(handler, Command::ADD_USER, { name });
     return result.get<std::string>("id");
 }
 
-std::string Forum::Helpers::createDiscussionThreadAndGetId(CommandHandlerRef handler, const std::string& name)
+std::string Forum::Helpers::createDiscussionThreadAndGetId(CommandHandlerRef& handler, const std::string& name)
 {
     auto result = handlerToObj(handler, Command::ADD_DISCUSSION_THREAD, { name });
     return result.get<std::string>("id");
 }
 
-std::string Forum::Helpers::createDiscussionMessageAndGetId(CommandHandlerRef handler, const std::string& threadId,
+std::string Forum::Helpers::createDiscussionMessageAndGetId(CommandHandlerRef& handler, const std::string& threadId,
                                                             const std::string& content)
 {
     auto result = handlerToObj(handler, Command::ADD_DISCUSSION_THREAD_MESSAGE, { threadId, content });
     return result.get<std::string>("id");
 }
 
-std::string Forum::Helpers::createDiscussionTagAndGetId(CommandHandlerRef handler, const std::string& name)
+std::string Forum::Helpers::createDiscussionTagAndGetId(CommandHandlerRef& handler, const std::string& name)
 {
     auto result = handlerToObj(handler, Command::ADD_DISCUSSION_TAG, { name });
     return result.get<std::string>("id");
 }
 
-std::string Forum::Helpers::createDiscussionCategoryAndGetId(CommandHandlerRef handler, const std::string& name,
+std::string Forum::Helpers::createDiscussionCategoryAndGetId(CommandHandlerRef& handler, const std::string& name,
                                                              const std::string& parentId)
 {
     auto result = handlerToObj(handler, Command::ADD_DISCUSSION_CATEGORY, { name, parentId });
     return result.get<std::string>("id");
 }
 
-void Forum::Helpers::deleteDiscussionThread(CommandHandlerRef handler, const std::string& id)
+void Forum::Helpers::deleteDiscussionThread(CommandHandlerRef& handler, const std::string& id)
 {
     handlerToObj(handler, Command::DELETE_DISCUSSION_THREAD, { id });
 }
 
-void Forum::Helpers::deleteDiscussionThreadMessage(CommandHandlerRef handler, const std::string& id)
+void Forum::Helpers::deleteDiscussionThreadMessage(CommandHandlerRef& handler, const std::string& id)
 {
     handlerToObj(handler, Command::DELETE_DISCUSSION_THREAD_MESSAGE, { id });
 }
 
-void Forum::Helpers::deleteDiscussionTag(CommandHandlerRef handler, const std::string& id)
+void Forum::Helpers::deleteDiscussionTag(CommandHandlerRef& handler, const std::string& id)
 {
     handlerToObj(handler, Command::DELETE_DISCUSSION_TAG, { id });
 }
--- test/ForumServiceTests/CommandsCommon.h
+++ test/ForumServiceTests/CommandsCommon.h
@@ -18,9 +18,9 @@ namespace Forum
     {
         Commands::CommandHandlerRef createCommandHandler();
 
-        std::string handlerToString(Commands::CommandHandlerRef handler, Commands::Command command,
+        std::string handlerToString(Commands::CommandHandlerRef& handler, Commands::Command command,
                                     const std::vector<std::string>& parameters);
-        std::string handlerToString(Commands::CommandHandlerRef handler, Commands::Command command);
+        std::string handlerToString(Commands::CommandHandlerRef& handler, Commands::Command command);
 
         struct DisplaySettings
         {
@@ -32,27 +32,27 @@ namespace Forum
             DisplaySettings(Context::SortOrder overrideSortOrder) : sortOrder(overrideSortOrder) {}
         };
 
-        boost::property_tree::ptree handlerToObj(Commands::CommandHandlerRef handler,
+        boost::property_tree::ptree handlerToObj(Commands::CommandHandlerRef& handler,
                                                  Commands::Command command,
                                                  const std::vector<std::string>& parameters);
-        boost::property_tree::ptree handlerToObj(Commands::CommandHandlerRef handler,
+        boost::property_tree::ptree handlerToObj(Commands::CommandHandlerRef& handler,
                                                  Commands::Command command, DisplaySettings displaySettings,
                                                  const std::vector<std::string>& parameters);
-        boost::property_tree::ptree handlerToObj(Commands::CommandHandlerRef handler,
+        boost::property_tree::ptree handlerToObj(Commands::CommandHandlerRef& handler,
                                                  Commands::Command command);
-        boost::property_tree::ptree handlerToObj(Commands::CommandHandlerRef handler,
+        boost::property_tree::ptree handlerToObj(Commands::CommandHandlerRef& handler,
                                                  Commands::Command command, DisplaySettings displaySettings);
 
-        std::string createUserAndGetId(Commands::CommandHandlerRef handler, const std::string& name);
-        std::string createDiscussionThreadAndGetId(Commands::CommandHandlerRef handler, const std::string& name);
-        std::string createDiscussionMessageAndGetId(Commands::CommandHandlerRef handler,
+        std::string createUserAndGetId(Commands::CommandHandlerRef& handler, const std::string& name);
+        std::string createDiscussionThreadAndGetId(Commands::CommandHandlerRef& handler, const std::string& name);
+        std::string createDiscussionMessageAndGetId(Commands::CommandHandlerRef& handler,
                                                     const std::string& threadId, const std::string& content);
-        std::string createDiscussionTagAndGetId(Commands::CommandHandlerRef handler, const std::string& name);
-        std::string createDiscussionCategoryAndGetId(Commands::CommandHandlerRef handler, const std::string& name,
+        std::string createDiscussionTagAndGetId(Commands::CommandHandlerRef& handler, const std::string& name);
+        std::string createDiscussionCategoryAndGetId(Commands::CommandHandlerRef& handler, const std::string& name,
                                                      const std::string& parentId = "");
-        void deleteDiscussionThread(Commands::CommandHandlerRef handler, const std::string& id);
-        void deleteDiscussionThreadMessage(Commands::CommandHandlerRef handler, const std::string& id);
-        void deleteDiscussionTag(Commands::CommandHandlerRef handler, const std::string& id);
+        void deleteDiscussionThread(Commands::CommandHandlerRef& handler, const std::string& id);
+        void deleteDiscussionThreadMessage(Commands::CommandHandlerRef& handler, const std::string& id);
+        void deleteDiscussionTag(Commands::CommandHandlerRef& handler, const std::string& id);
 
         template<typename T, typename It>
         void fillPropertyFromCollection(const boost::property_tree::ptree& collection, const char* name,
