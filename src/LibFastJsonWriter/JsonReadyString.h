e61d2a681014f966e7898005a73a93c033a94238
Dani J <dani.user@gmail.com>
2017-08-28 21:28:56 +0300

    Refactored JsonReadyString to make it more extendable

--- src/LibFastJsonWriter/JsonReadyString.h
+++ src/LibFastJsonWriter/JsonReadyString.h
@@ -1,150 +1,123 @@
 #pragma once
 
-#include <algorithm>
+#include "StringContainer.h"
+
 #include <cassert>
-#include <cmath>
-#include <cstddef>
 #include <cstdint>
 
 #include <boost/utility/string_view.hpp>
 
 namespace Json
 {
-    template<size_t StackSize>
-    class JsonReadyString
+    struct SizeWithBool
+    {
+        uint32_t boolean: 1;
+        uint32_t size   :31;
+
+        SizeWithBool() : boolean(0), size(0) {}
+        SizeWithBool(size_t size) : boolean(0), size(static_cast<decltype(size)>(size)) {}
+
+        SizeWithBool(const SizeWithBool&) = default;
+        SizeWithBool& operator=(const SizeWithBool&) = default;
+
+        explicit operator size_t() const noexcept 
+        {
+            return static_cast<size_t>(size);
+        }
+
+        SizeWithBool& operator=(size_t value) noexcept 
+        {
+            size = static_cast<decltype(size)>(value);
+            return *this;
+        }
+
+        explicit operator bool() const noexcept
+        {
+            return boolean != 0;
+        }
+
+        SizeWithBool& operator=(bool value) noexcept
+        {
+            boolean = value ? 1 : 0;
+            return *this;
+        }
+    };
+
+    template<size_t StackSize, typename Derived, typename SizeType = SizeWithBool>
+    class JsonReadyStringBase
     {
     public:
-        explicit JsonReadyString(boost::string_view source);
-        ~JsonReadyString();
+        explicit JsonReadyStringBase(boost::string_view source);
+        JsonReadyStringBase(const JsonReadyStringBase&) = default;
+        JsonReadyStringBase(JsonReadyStringBase&&) = default;
 
-        JsonReadyString(const JsonReadyString& other);
-        JsonReadyString(JsonReadyString&& other) noexcept;
-
-        JsonReadyString& operator=(JsonReadyString other);
-        JsonReadyString& operator=(JsonReadyString&& other) noexcept;
-
-        bool needsJsonEscape() const noexcept;
+        JsonReadyStringBase& operator=(const JsonReadyStringBase&) = default;
+        JsonReadyStringBase& operator=(JsonReadyStringBase&&) = default;
         
+        bool needsJsonEscape() const noexcept;
+
         boost::string_view string() const noexcept;
         boost::string_view quotedString() const noexcept;
 
-        friend void swap(JsonReadyString& first, JsonReadyString& second) noexcept
-        {
-            for (size_t i = 0; i < StackSize; ++i)
-            {
-                std::swap(first.buffer.stack[i], second.buffer.stack[i]);
-            }
-            std::swap(first.buffer.external, second.buffer.external);
-            std::swap(first.size, second.size);
-        }
-
-    private:
-        const char* stringStart() const noexcept;
-
-        union
-        {
-            char stack[StackSize];
-            char* external = nullptr;
-        } buffer;
-        /**
-         * Includes the size of the quotes if present.
-         * Positive if no escaping needed, negative if escaping needed.
-         */
-        int32_t size = 0;
+    protected:
+        StringContainer<StackSize, SizeType> container_;
     };
 
     bool isEscapeNeeded(const char* value, size_t length);
 
-    template <size_t StackSize>
-    JsonReadyString<StackSize>::JsonReadyString(boost::string_view source)
+    template<size_t StackSize>
+    class JsonReadyString : public JsonReadyStringBase<StackSize, JsonReadyString<StackSize>>
     {
-        char* destination = buffer.stack;
+    public:
+        explicit JsonReadyString(boost::string_view source);
+        JsonReadyString(const JsonReadyString&) = default;
+        JsonReadyString(JsonReadyString&&) = default;
+
+        JsonReadyString& operator=(const JsonReadyString&) = default;
+        JsonReadyString& operator=(JsonReadyString&&) = default;
+
+        static size_t extraBytesNeeded(boost::string_view source);
+    };
+
+    template <size_t StackSize, typename Derived, typename SizeType>
+    JsonReadyStringBase<StackSize, Derived, SizeType>::JsonReadyStringBase(boost::string_view source)
+    {
+        auto bytesNeeded = source.size();
         auto sourceSize = source.size();
 
-        if (isEscapeNeeded(source.data(), sourceSize))
-        {
-            if (sourceSize > StackSize)
-            {
-                destination = buffer.external = new char[sourceSize];
-            }
-            size = -static_cast<decltype(size)>(sourceSize);
-        }
-        else
-        {
-            //also use add quotes
-            auto destinationSize = sourceSize + 2;
-            size = static_cast<decltype(size)>(destinationSize);
+        auto escapeNeeded = isEscapeNeeded(source.data(), sourceSize);
 
-            if (destinationSize > StackSize)
-            {
-                destination = buffer.external = new char[destinationSize];
-            }            
-            *destination = '"';
-            destination[destinationSize - 1] = '"';
-            destination += 1;
+        if ( ! escapeNeeded)
+        {
+            bytesNeeded += 2; //start and end quotes
         }
 
+        bytesNeeded += Derived::extraBytesNeeded(source);
+
+        container_.resize(bytesNeeded);
+        container_.size() = escapeNeeded;
+
+        auto destination = *container_;
+
+        if ( ! escapeNeeded)
+        {
+            *destination++ = '"';
+            destination[sourceSize] = '"';
+        }
         std::copy(source.begin(), source.end(), destination);
     }
 
-    template <size_t StackSize>
-    JsonReadyString<StackSize>::~JsonReadyString()
+    template <size_t StackSize, typename Derived, typename SizeType>
+    bool JsonReadyStringBase<StackSize, Derived, SizeType>::needsJsonEscape() const noexcept
     {
-        if ((static_cast<size_t>(std::abs(size)) > StackSize) && buffer.external)
-        {
-            delete[] buffer.external;
-            buffer.external = nullptr;
-        }
+        return static_cast<bool>(container_.size());
     }
 
-    template <size_t StackSize>
-    JsonReadyString<StackSize>::JsonReadyString(const JsonReadyString& other)
+    template <size_t StackSize, typename Derived, typename SizeType>
+    boost::string_view JsonReadyStringBase<StackSize, Derived, SizeType>::string() const noexcept
     {
-        size = other.size;
-        const char* source = other.buffer.stack;
-        char* destination = buffer.stack;
-        auto absSize = std::abs(size);
-
-        if (absSize > StackSize)
-        {
-            destination = buffer.external = new char[absSize];
-            source = other.buffer.external;
-        }
-
-        std::copy(source, source + absSize, destination);
-    }
-
-    template <size_t StackSize>
-    JsonReadyString<StackSize>::JsonReadyString(JsonReadyString&& other) noexcept
-    {
-        std::swap(*this, other);
-    }
-
-    template <size_t StackSize>
-    JsonReadyString<StackSize>& JsonReadyString<StackSize>::operator=(JsonReadyString other)
-    {
-        std::swap(*this, other);
-        return *this;
-    }
-
-    template <size_t StackSize>
-    JsonReadyString<StackSize>& JsonReadyString<StackSize>::operator=(JsonReadyString&& other) noexcept
-    {
-        std::swap(*this, other);
-        return *this;
-    }
-
-    template <size_t StackSize>
-    bool JsonReadyString<StackSize>::needsJsonEscape() const noexcept
-    {
-        return size < 0;
-    }
-
-    template <size_t StackSize>
-    boost::string_view JsonReadyString<StackSize>::string() const noexcept
-    {
-        auto strStart = stringStart();
-        auto strSize = static_cast<boost::string_view::size_type>(std::abs(size));
+        auto strStart = *container_;
+        auto strSize = static_cast<boost::string_view::size_type>(container_.size());
 
         if ( ! needsJsonEscape())
         {
@@ -156,8 +129,8 @@ namespace Json
         return boost::string_view(strStart, strSize);
     }
 
-    template <size_t StackSize>
-    boost::string_view JsonReadyString<StackSize>::quotedString() const noexcept
+    template <size_t StackSize, typename Derived, typename SizeType>
+    boost::string_view JsonReadyStringBase<StackSize, Derived, SizeType>::quotedString() const noexcept
     {
         if (needsJsonEscape())
         {
@@ -165,12 +138,18 @@ namespace Json
             return{};
         }
 
-        return boost::string_view(stringStart(), static_cast<boost::string_view::size_type>(std::abs(size)));
+        return boost::string_view(*container_, static_cast<boost::string_view::size_type>(container_.size()));
     }
 
     template <size_t StackSize>
-    const char* JsonReadyString<StackSize>::stringStart() const noexcept
+    JsonReadyString<StackSize>::JsonReadyString(boost::string_view source)
+        : JsonReadyStringBase<StackSize, JsonReadyString<StackSize>>(source)
     {
-        return (std::abs(size) <= StackSize) ? buffer.stack : buffer.external;
+    }
+
+    template <size_t StackSize>
+    size_t JsonReadyString<StackSize>::extraBytesNeeded(boost::string_view source)
+    {
+        return 0;
     }
 }
---
+++ src/LibFastJsonWriter/StringContainer.h
@@ -0,0 +1,149 @@
+#pragma once
+
+#include <algorithm>
+#include <cstddef>
+
+namespace Json
+{
+    template<size_t StackSize, typename SizeType>
+    class StringContainer
+    {
+    public:
+        StringContainer();
+        explicit StringContainer(size_t size);
+        ~StringContainer();
+
+        StringContainer(const StringContainer& other);
+        StringContainer(StringContainer&& other) noexcept;
+
+        StringContainer& operator=(const StringContainer& other);
+        StringContainer& operator=(StringContainer&& other) noexcept;
+
+        void resize(size_t newSize);
+
+        char* operator*() noexcept;
+        const char* operator*() const noexcept;
+
+        SizeType& size() noexcept;
+        SizeType size() const noexcept;
+
+        friend void swap(StringContainer& first, StringContainer& second) noexcept
+        {
+            for (size_t i = 0; i < StackSize; ++i)
+            {
+                std::swap(first.buffer_.stack[i], second.buffer_.stack[i]);
+            }
+            std::swap(first.size_, second.size_);
+        }
+
+    private:
+        bool usingHeapBuffer() const noexcept;
+
+        union
+        {
+            char stack[StackSize];
+            char* external = nullptr;
+        } buffer_;
+
+        SizeType size_;
+    };
+
+    template <size_t StackSize, typename SizeType>
+    StringContainer<StackSize, SizeType>::StringContainer() : size_({})
+    {
+    }
+
+    template <size_t StackSize, typename SizeType>
+    StringContainer<StackSize, SizeType>::StringContainer(size_t size) : size_(size)
+    {
+        if (usingHeapBuffer())
+        {
+            buffer_.external = new char[size];
+        }
+    }
+
+    template <size_t StackSize, typename SizeType>
+    StringContainer<StackSize, SizeType>::~StringContainer()
+    {
+        if (usingHeapBuffer() && buffer_.external)
+        {
+            delete[] buffer_.external;
+            buffer_.external = nullptr;
+        }
+    }
+
+    template <size_t StackSize, typename SizeType>
+    StringContainer<StackSize, SizeType>::StringContainer(const StringContainer& other)
+        : size_(other.size_)
+    {
+        auto source = other.buffer_.stack;
+        auto destination = buffer_.stack;
+        auto size = static_cast<size_t>(size_);
+
+        if (usingHeapBuffer())
+        {
+            destination = buffer_.external = new char[size];
+            source = other.buffer_.external;
+        }
+
+        std::copy(source, source + size, destination);
+    }
+
+    template <size_t StackSize, typename SizeType>
+    StringContainer<StackSize, SizeType>::StringContainer(StringContainer&& other) noexcept
+    {
+        swap(*this, other);
+    }
+
+    template <size_t StackSize, typename SizeType>
+    StringContainer<StackSize, SizeType>& StringContainer<StackSize, SizeType>::operator=(const StringContainer& other)
+    {
+        StringContainer<StackSize, SizeType> copy(other);
+        swap(*this, copy);
+        return *this;
+    }
+
+    template <size_t StackSize, typename SizeType>
+    StringContainer<StackSize, SizeType>& StringContainer<StackSize, SizeType>::operator=(StringContainer&& other) noexcept
+    {
+        swap(*this, other);
+        return *this;
+    }
+
+    template <size_t StackSize, typename SizeType>
+    void StringContainer<StackSize, SizeType>::resize(size_t newSize)
+    {
+        StringContainer<StackSize, SizeType> other(newSize);
+        swap(*this, other);
+    }
+
+    template <size_t StackSize, typename SizeType>
+    char* StringContainer<StackSize, SizeType>::operator*() noexcept
+    {
+        return usingHeapBuffer() ? buffer_.external : buffer_.stack;
+    }
+
+    template <size_t StackSize, typename SizeType>
+    const char* StringContainer<StackSize, SizeType>::operator*() const noexcept
+    {
+        return usingHeapBuffer() ? buffer_.external : buffer_.stack;
+    }
+
+    template <size_t StackSize, typename SizeType>
+    SizeType& StringContainer<StackSize, SizeType>::size() noexcept
+    {
+        return size_;
+    }
+
+    template <size_t StackSize, typename SizeType>
+    SizeType StringContainer<StackSize, SizeType>::size() const noexcept
+    {
+        return size_;
+    }
+
+    template <size_t StackSize, typename SizeType>
+    bool StringContainer<StackSize, SizeType>::usingHeapBuffer() const noexcept
+    {
+        return static_cast<size_t>(size_) > StackSize;
+    }
+}
