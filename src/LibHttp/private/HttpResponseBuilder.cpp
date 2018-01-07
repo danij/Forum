cf1ae9b78e3c1cc1ec3836234f69ef62b1544c37
Dani J <dani.user@gmail.com>
2018-01-07 16:03:18 +0200

    Updated copyright notice

--- src/LibHttp/private/HttpResponseBuilder.cpp
+++ src/LibHttp/private/HttpResponseBuilder.cpp
@@ -1,6 +1,6 @@
 /*
 Fast Forum Backend
-Copyright (C) 2016-2017 Daniel Jurcau
+Copyright (C) 2016-present Daniel Jurcau
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
