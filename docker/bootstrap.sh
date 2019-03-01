66761c413d62b96a52e565ea687b886d8af42295
Dani J <dani.user@gmail.com>
2019-03-01 21:03:46 +0200

    Added bootstrap script for container

---
+++ docker/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir /forum/config/Forum
+mkdir /forum/data/events
+mkdir /forum/data/search_updates
+mkdir /forum/logs/Forum
+
+cp /forum/repos/Forum/forum_settings-example.json /forum/config/Forum/forum_settings.json
+sed -i 's#log.settings#/forum/config/Forum/log.settings#' /forum/config/Forum/forum_settings.json
+sed -i 's#/mnt/forum/input#/forum/data/events#' /forum/config/Forum/forum_settings.json
+sed -i 's#/mnt/forum/output#/forum/data/events#' /forum/config/Forum/forum_settings.json
+sed -i 's#ForumSearchUpdatePlugin.so#/forum/repos/Forum-RelWithDebInfo/plugins/ForumSearchUpdatePlugin/libForumSearchUpdatePlugin.so#' /forum/config/Forum/forum_settings.json
+sed -i 's#search_updates-%1%.json#/forum/data/search_updates/search_updates-%1%.json#' /forum/config/Forum/forum_settings.json
+
+cp /forum/repos/Forum/example-log.settings /forum/config/Forum/log.settings
+sed -i 's#file_%Y%m%d.log#/forum/logs/Forum/%Y%m%d.log#' /forum/config/Forum/log.settings
