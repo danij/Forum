f645266dcd25b28302c86855db8d3c688e8a0881
Dani J <dani.user@gmail.com>
2019-03-04 19:11:17 +0200

    Replace the hostname when bootstrapping a container

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+HOSTNAME="$1"
+
 mkdir /forum/config/Forum
 mkdir /forum/data/events
 mkdir /forum/data/search_updates
@@ -10,6 +12,7 @@ sed -i 's#/mnt/forum/input#/forum/data/events#' /forum/config/Forum/forum_settin
 sed -i 's#/mnt/forum/output#/forum/data/events#' /forum/config/Forum/forum_settings.json
 sed -i 's#ForumSearchUpdatePlugin.so#/forum/repos/Forum-RelWithDebInfo/plugins/ForumSearchUpdatePlugin/libForumSearchUpdatePlugin.so#' /forum/config/Forum/forum_settings.json
 sed -i 's#search_updates-%1%.json#/forum/data/search_updates/search_updates-%1%.json#' /forum/config/Forum/forum_settings.json
+sed -i "s#dani.forum#$HOSTNAME#" /forum/config/Forum/forum_settings.json
 
 cp /forum/repos/Forum/example-log.settings /forum/config/Forum/log.settings
 sed -i 's#file_%Y%m%d.log#/forum/logs/Forum/%Y%m%d.log#' /forum/config/Forum/log.settings
