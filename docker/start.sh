c0a865a2d45b6c077a81109a0aeedc39bf2822be
Dani J <dani.user@gmail.com>
2019-03-11 19:37:33 +0200

    Added startup script

---
+++ docker/start.sh
@@ -0,0 +1,9 @@
+su -c '/usr/lib/postgresql/10/bin/pg_ctl -D /forum/data/sqldb -l /forum/logs/sqldb/logfile start' postgres
+service nginx start
+service cron start
+
+/forum/start/Forum.Auth.sh &
+/forum/start/Forum.Attachments.sh &
+/forum/start/Forum.Search.sh &
+
+/forum/repos/Forum-RelWithDebInfo/src/ForumApp/ForumApp -c /forum/config/Forum/forum_settings.json
